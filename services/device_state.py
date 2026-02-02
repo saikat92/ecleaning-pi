621c7bd57c175e52099c3cde020b1846438f42c2
saikat92 <say.picklu@gmail.com>
2026-02-03 00:03:02 +0530

    V 3.1.0

--- services/device_state.py
+++ services/device_state.py
@@ -1,8 +1,18 @@
 class DeviceState:
-    IDLE = "idle"
-    CLEANING = "cleaning"
+    IDLE = "Idle"
+    MACHINE_ON = "Machine On"
+    AUTO_CLEANING = "Automatic Cleaning Mode selected"
+    MANUAL_CLEANING = "Manual Cleaning Mode selected"
+    UV_ON = "UV Light Turned On"
+    UV_OFF = "UV Light Turned Off"
+    CONV_ON = "Conveyor Running.."
+    CONV_OFF = "Conveyor Stopped"
     PAUSED = "paused"
     ERROR = "error"
+    CALLIBRATE = "Checking System..."
+    RESTART = "Restarting system..."
+    CLEANING_START = "Cleaning Started"
+    CLEANING_COMPLETE = "Cleaning completes. Collect Packet"
 
     def __init__(self):
         self.state = self.IDLE
--- services/mqtt_test.py
+++ services/mqtt_test.py
@@ -11,11 +11,30 @@ def on_command(cmd):
         action = data.get("action")
 
         if action == "start":
-            device.set_state(DeviceState.CLEANING)
+            device.set_state(DeviceState.MACHINE_ON)
         elif action == "pause":
             device.set_state(DeviceState.PAUSED)
         elif action == "stop":
             device.set_state(DeviceState.IDLE)
+        elif action == "auto_clean":
+            device.set_state(DeviceState.AUTO_CLEANING)
+        elif action == "man_clean":
+            device.set_state(DeviceState.MANUAL_CLEANING)
+        elif action == "uv_on":
+            device.set_state(DeviceState.UV_ON)
+        elif action == "uv_off":
+            device.set_state(DeviceState.UV_OFF)
+        elif action == "conv_on":
+            device.set_state(DeviceState.CONV_ON)
+        elif action == "conv_off":
+            device.set_state(DeviceState.CONV_OFF)
+        elif action == "calibrate":
+            device.set_state(DeviceState.CALLIBRATE)
+        elif action == "restart":
+            device.set_state(DeviceState.RESTART)
+        elif action == "clean_start":
+            device.set_state(DeviceState.CLEANING_START)
+        
         else:
             print("[APP] Unknown command:", action)
 
